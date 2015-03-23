14f8faedd36296b336697a5bf3a2073b902f54fe
Igor Moochnick <imoochnick@constantcontact.com>
2015-03-23 09:46:26 -0400

    Making the HystrixCommand method public

--- code/java/11_consumerCircuitBreaker/src/main/java/net/ctct/mysvc/service/ProducerClient.java
+++ code/java/11_consumerCircuitBreaker/src/main/java/net/ctct/mysvc/service/ProducerClient.java
@@ -5,23 +5,18 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.stereotype.Component;
 
 @Component
-//@EnableCircuitBreaker
-//@EnableHystrix
 public class ProducerClient {
 
 //    @Autowired
 //    RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "getProducerFallback")
-//            commandProperties = {
-//                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
-//            })
-    ProducerResponse getProducerResponse() {
+    public ProducerResponse getProducerResponse() {
         return new ProducerResponse();
 //        return restTemplate.getForObject("http://producer", ProducerResponse.class);
     }
 
-    ProducerResponse getProducerFallback() {
+    public ProducerResponse getProducerFallback() {
         ProducerResponse producerResponse = new ProducerResponse();
         producerResponse.value = 42;
         return producerResponse;
