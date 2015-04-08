5b38bc5cf14cfbaf51021214d281126d834155b4
Igor Moochnick <imoochnick@constantcontact.com>
2015-04-08 00:27:09 -0400

    Cleaned up config server

--- code/java/03_configServer/src/main/java/net/ctct/mysvc/Application.java
+++ code/java/03_configServer/src/main/java/net/ctct/mysvc/Application.java
@@ -1,18 +1,18 @@
 package net.ctct.mysvc;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-//@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setShowBanner(false);
-        app.run(args);
+        new SpringApplicationBuilder(Application.class)
+//                .properties("spring.config.name=configserver")
+                .showBanner(false)
+                .run(args);
     }
 
 }
