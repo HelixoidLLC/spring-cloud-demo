4f14897bf5a5214a4e9deaf09ce88088784df5a8
Igor Moochnick <imoochnick@constantcontact.com>
2015-04-08 00:31:19 -0400

    Cleaned up an unused variable

--- code/java/10_consumerFeign/src/main/java/net/ctct/mysvc/service/SampleController.java
+++ code/java/10_consumerFeign/src/main/java/net/ctct/mysvc/service/SampleController.java
@@ -3,7 +3,6 @@ package net.ctct.mysvc.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,9 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
 
     private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
-
-    @Autowired
-    LoadBalancerClient loadBalancer;
 
     @Autowired
     ProducerClient client;
