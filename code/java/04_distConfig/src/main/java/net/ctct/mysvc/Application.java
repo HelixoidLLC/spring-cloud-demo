599127e36b61fb65b0aea2772c40c63cf5126c3f
Igor Moochnick <imoochnick@constantcontact.com>
2015-04-08 00:28:24 -0400

    Fixed refresh scope for the distributed settings refresh

--- code/java/04_distConfig/src/main/java/net/ctct/mysvc/Application.java
+++ code/java/04_distConfig/src/main/java/net/ctct/mysvc/Application.java
@@ -1,22 +1,15 @@
 package net.ctct.mysvc;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
-// @Configuration
-// @EnableAutoConfiguration
-// @ComponentScan
-// Equivalent:
 @SpringBootApplication
-@EnableConfigServer
-//@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Application.class);
-        app.setShowBanner(false);
-        app.run(args);
+        new SpringApplicationBuilder(Application.class)
+                .showBanner(false)
+                .run(args);
     }
 
 }
--- code/java/04_distConfig/src/main/java/net/ctct/mysvc/service/SampleController.java
+++ code/java/04_distConfig/src/main/java/net/ctct/mysvc/service/SampleController.java
@@ -1,23 +1,22 @@
 package net.ctct.mysvc.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RefreshScope
 public class SampleController {
 
-//    @Value("${greeting}")
-    //String greeting = "Hello";
-
-    @Autowired
-    Greeter greeter;
+    @Value("${greeting: Fun}")
+    String greeting;
 
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return String.format("%s, World!", greeter.greeting);
+        return String.format("%s, World!", greeting);
     }
 
 }
