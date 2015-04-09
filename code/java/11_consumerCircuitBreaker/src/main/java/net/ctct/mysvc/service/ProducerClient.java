8f1b9545c9defbe51f36c15d7e81348f7fb578cf
Igor Moochnick <imoochnick@constantcontact.com>
2015-04-08 21:32:55 -0400

    Fixed circuit breakers demo

--- code/java/11_consumerCircuitBreaker/src/main/java/net/ctct/mysvc/service/ProducerClient.java
+++ code/java/11_consumerCircuitBreaker/src/main/java/net/ctct/mysvc/service/ProducerClient.java
@@ -1,19 +1,19 @@
 package net.ctct.mysvc.service;
 
-
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ProducerClient {
 
-//    @Autowired
-//    RestTemplate restTemplate;
+    @Autowired
+    RestTemplate restTemplate;
 
     @HystrixCommand(fallbackMethod = "getProducerFallback")
     public ProducerResponse getProducerResponse() {
-        return new ProducerResponse();
-//        return restTemplate.getForObject("http://producer", ProducerResponse.class);
+        return restTemplate.getForObject("http://producer", ProducerResponse.class);
     }
 
     public ProducerResponse getProducerFallback() {
